90ca63a2d4c79d1a1979cd1d76b635bdebecbb73
Daniel Ecer <de-code@users.noreply.github.com>
2020-10-07 21:38:49 +0100

    minimal project structure

---
+++ tests/cli_test.py
@@ -0,0 +1,6 @@
+from tf_bodypix.cli import main
+
+
+class TestMain:
+    def test_should_not_fail(self):
+        main()
---
+++ tf_bodypix/cli.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+def main(argv: List[str] = None):  # pylint: disable=unused-argument
+    print('coming soon')
+
+
+if __name__ == '__main__':
+    main()
