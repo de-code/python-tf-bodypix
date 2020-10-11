c1f8f16d76a98d4947976d0cb6108208ae70bb61
Daniel Ecer <de-code@users.noreply.github.com>
2020-10-11 01:39:04 +0100

    made casting conditional

--- tf_bodypix/cli.py
+++ tf_bodypix/cli.py
@@ -126,8 +126,10 @@ class ImageToMaskSubCommand(SubCommand):
             timer.on_step_start('overlay')
             LOGGER.debug('mask.shape: %s (%s)', mask.shape, mask.dtype)
             alpha = args.add_overlay_alpha
+            if mask.dtype == tf.int32:
+                mask = tf.cast(mask, tf.float32)
             output = np.clip(
-                image_array + tf.cast(mask, tf.float32) * alpha,
+                image_array + mask * alpha,
                 0.0, 255.0
             )
             return output
