968b71b653d718922cdaf1101dcbf49f4b087953
Daniel Ecer <de-code@users.noreply.github.com>
2020-10-14 01:27:12 +0200

    added args for source image size and fourcc (#23)
    * added args for source image size and fourcc
    
    * slightly improved help text

--- tf_bodypix/utils/opencv.py
+++ tf_bodypix/utils/opencv.py
@@ -5,21 +5,51 @@ from typing import ContextManager, Iterable
 import cv2
 import numpy as np
 
+from tf_bodypix.utils.image import ImageSize, resize_image_to
+
 
 LOGGER = logging.getLogger(__name__)
 
 
-def iter_read_video_images(video_capture: cv2.VideoCapture) -> Iterable[np.ndarray]:
+def iter_read_video_images(
+    video_capture: cv2.VideoCapture,
+    image_size: ImageSize = None
+) -> Iterable[np.ndarray]:
+    is_first = True
     while True:
         _, image_array = video_capture.read()
         LOGGER.debug('cam image_array.shape: %s', image_array.shape)
+        if is_first:
+            LOGGER.info(
+                'received webcam image shape: %s (requested: %s)',
+                image_array.shape, image_size
+            )
+        if image_size:
+            image_array = resize_image_to(image_array, image_size)
         yield image_array
+        is_first = False
 
 
 @contextmanager
-def get_webcam_image_source(webcam_number: int) -> ContextManager[Iterable[np.ndarray]]:
+def get_webcam_image_source(
+    webcam_number: int,
+    image_size: ImageSize = None,
+    fourcc: str = None
+) -> ContextManager[Iterable[np.ndarray]]:
     cam = cv2.VideoCapture(webcam_number)
+    if fourcc:
+        LOGGER.info('setting camera fourcc to %r', fourcc)
+        cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*fourcc))
     cam.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+    if image_size:
+        LOGGER.info('attempting to set camera image size to: %s', image_size)
+        cam.set(cv2.CAP_PROP_FRAME_WIDTH, image_size.width)
+        cam.set(cv2.CAP_PROP_FRAME_HEIGHT, image_size.height)
+    actual_camera_image_size = ImageSize(
+        width=cam.get(cv2.CAP_PROP_FRAME_WIDTH),
+        height=cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    )
+    LOGGER.info('camera reported image size: %s', actual_camera_image_size)
     try:
         yield iter_read_video_images(cam)
     finally:
