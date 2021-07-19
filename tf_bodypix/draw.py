f0a19deaf028dada680b65c0c210a4219ace15c7
Daniel Ecer <de-code@users.noreply.github.com>
2021-07-19 22:01:39 +0100

    fixed KeyPoint size argument

--- tf_bodypix/draw.py
+++ tf_bodypix/draw.py
@@ -45,7 +45,7 @@ def get_cv_keypoints(keypoints: Iterable[Keypoint]) -> List[cv2.KeyPoint]:
         cv2.KeyPoint(
             x=keypoint.position.x,
             y=keypoint.position.y,
-            _size=3
+            size=3
         )
         for keypoint in keypoints
     ]
