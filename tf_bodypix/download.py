a1b4e2a073bb3cfa0b6bc1c63ad18b6f66c0e861
Daniel Ecer <de-code@users.noreply.github.com>
2021-11-22 23:29:45 +0000

    replaced tf.keras.utils.get_file with requests call

--- tf_bodypix/download.py
+++ tf_bodypix/download.py
@@ -5,7 +5,7 @@ import re
 
 from hashlib import md5
 
-import tensorflow as tf
+from tf_bodypix.utils.io import download_file_to, get_default_cache_dir
 
 
 LOGGER = logging.getLogger(__name__)
@@ -70,11 +70,13 @@ def download_model(model_path: str) -> str:
         + os.path.basename(local_name_part)
     )
     LOGGER.debug('local_name: %r', local_name)
-    cache_subdir = os.path.join('tf-bodypix', local_name)
-    local_model_json_path = tf.keras.utils.get_file(
-        'model.json',
-        model_path,
-        cache_subdir=cache_subdir,
+    cache_dir = get_default_cache_dir(
+        cache_subdir=os.path.join('tf-bodypix', local_name)
+    )
+    local_model_json_path = download_file_to(
+        source_url=model_path,
+        local_path=os.path.join(cache_dir, 'model.json'),
+        skip_if_exists=True
     )
     local_model_path = os.path.dirname(local_model_json_path)
     LOGGER.debug('local_model_json_path: %r', local_model_json_path)
@@ -100,9 +102,9 @@ def download_model(model_path: str) -> str:
     })
     LOGGER.debug('weights_manifest_paths: %s', weights_manifest_paths)
     for weights_manifest_path in weights_manifest_paths:
-        local_model_json_path = tf.keras.utils.get_file(
-            os.path.basename(weights_manifest_path),
-            model_base_path + '/' + weights_manifest_path,
-            cache_subdir=cache_subdir,
+        local_model_json_path = download_file_to(
+            source_url=model_base_path + '/' + weights_manifest_path,
+            local_path=os.path.join(cache_dir, os.path.basename(weights_manifest_path)),
+            skip_if_exists=True
         )
     return local_model_path
--- tf_bodypix/utils/io.py
+++ tf_bodypix/utils/io.py
@@ -1,7 +1,14 @@
 import os
 from hashlib import md5
+from pathlib import Path
+from typing import Optional
+
+import requests
 
 import tensorflow as tf
+
+
+DEFAULT_KERAS_CACHE_DIR = '~/.keras'
 
 
 def strip_url_suffix(path: str) -> str:
@@ -9,6 +16,29 @@ def strip_url_suffix(path: str) -> str:
     if qs_index > 0:
         return path[:qs_index]
     return path
+
+
+def get_default_cache_dir(
+    cache_dir: Optional[str] = None,
+    cache_subdir: Optional[str] = None
+):
+    result = os.path.expanduser(cache_dir or DEFAULT_KERAS_CACHE_DIR)
+    if cache_subdir:
+        result = os.path.join(result, cache_subdir)
+    return result
+
+
+def download_file_to(
+    source_url: str,
+    local_path: str,
+    skip_if_exists: bool = True
+):
+    if skip_if_exists and os.path.exists(local_path):
+        return local_path
+    response = requests.get(source_url)
+    response.raise_for_status()
+    Path(local_path).write_bytes(response.content)
+    return local_path
 
 
 def get_file(file_path: str, download: bool = True) -> str:
